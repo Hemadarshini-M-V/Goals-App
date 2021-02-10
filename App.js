d0c89781f1ffe01e0226916787cf5df25b1a2afd
Hemadarshini-M-V <hemadarshini.viswan@gmail.com>
2021-02-10 14:29:25 +0530

    Handling user input

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const changeEnteredGoal = (enteredText) => {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoal = () => {
+    setCourseGoals( currentGoals => 
+      [...currentGoals, {key: Math.random().toString(), 
+        goal: enteredGoal }]);
+    setEnteredGoal('');
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.goalInputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.goalInput} />
-        <Button title="Add" style={styles.addButton} />
+        <TextInput placeholder="Course Goal" 
+          style={styles.goalInput}
+          onChangeText={changeEnteredGoal} 
+          value={enteredGoal} />
+        <Button title="Add" 
+          style={styles.addButton} 
+          onPress={addGoal} />
       </View>
-      <View>
-
-      </View>
+      <FlatList 
+        data ={courseGoals}
+        renderItem = {(itemData)=>  
+          <Text style={styles.goalList} >
+            {itemData.item.goal}
+          </Text>}/>
     </View>
   );
 }
@@ -33,6 +55,13 @@ const styles = StyleSheet.create({
   },
   addButton: { 
     padding: 100 
+  },
+  goalList: {
+    marginVertical: 10,
+    padding: 10,
+    borderColor: 'blue',
+    borderWidth: 1,
+    backgroundColor: 'cyan'
   }
 });
 
